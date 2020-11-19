76539b2f44c75e2c92e97eda7972f9ea9c05d72b
Kevin Luo <kvn.luo@gmail.com>
2020-11-19 01:12:57 -0500

    remove unused variables

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,6 @@ import React from "react";
 import {
   Link,
   IconButton,
-  Divider,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import GitHubIcon from "@material-ui/icons/GitHub";
